e6a2a5e6de15f4c06ea7e325ff45b81eaeab9382
Simon Seo <simon250@hotmail.com>
2017-09-20 01:26:00 -0400

    updated MainActivity
    need to add logic for the calculations

--- src/main/java/comsimon_seo/github/binomcoeffcalc/MainActivity.java
+++ src/main/java/comsimon_seo/github/binomcoeffcalc/MainActivity.java
@@ -2,12 +2,30 @@ package comsimon_seo.github.binomcoeffcalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button_calculate;
+    TextView textView_output;
+    EditText editText_int_n, editText_int_k;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_calculate = (Button)findViewById(R.id.button_calculate);
+        textView_output = (TextView)findViewById(R.id.textView_output);
+        editText_int_n = (EditText)findViewById(R.id.editText_int_n);
+        editText_int_k = (EditText)findViewById(R.id.editText_int_k);
+
+        button_calculate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
     }
 }
