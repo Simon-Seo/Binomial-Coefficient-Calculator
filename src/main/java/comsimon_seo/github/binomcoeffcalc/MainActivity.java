b10048d38d8f8606184410bf71b29a1645f47005
Simon Seo <simon250@hotmail.com>
2017-09-22 01:40:56 -0400

    Fixed overflowing issues with large numbers. There is no longer a size limit for the output.
    Calculate button text altered from "CALCULATE NCK" to "CALCULATE"

--- src/main/java/comsimon_seo/github/binomcoeffcalc/MainActivity.java
+++ src/main/java/comsimon_seo/github/binomcoeffcalc/MainActivity.java
@@ -6,11 +6,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.math.BigInteger;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button button_calculate;
     TextView textView_output;
-    EditText editText_int_n, editText_int_k;
+    EditText editText_BigInteger_n, editText_BigInteger_k;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         button_calculate = (Button)findViewById(R.id.button_calculate);
         textView_output = (TextView)findViewById(R.id.textView_output);
-        editText_int_n = (EditText)findViewById(R.id.editText_int_n);
-        editText_int_k = (EditText)findViewById(R.id.editText_int_k);
+        editText_BigInteger_n = (EditText)findViewById(R.id.editText_BigInteger_n);
+        editText_BigInteger_k = (EditText)findViewById(R.id.editText_BigInteger_k);
 
         button_calculate.setOnClickListener(this);
     }
@@ -29,20 +30,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view){
         switch(view.getId()){
             case R.id.button_calculate:
-                String string_n = editText_int_n.getText().toString();
-                String string_k = editText_int_k.getText().toString();
-                int int_n = -1;
-                int int_k = 0;
+                String string_n = editText_BigInteger_n.getText().toString();
+                String string_k = editText_BigInteger_k.getText().toString();
+                BigInteger BigInteger_n = new BigInteger("-1");
+                BigInteger BigInteger_k = BigInteger.ZERO;
 
                 if (!string_n.equals("")&&!string_k.equals("")) {
-                    int_n = Integer.parseInt(string_n);
-                    int_k = Integer.parseInt(string_k);
+                    BigInteger_n = new BigInteger(string_n);
+                    BigInteger_k = new BigInteger(string_k);
                 }
 
                 String string_output;
-                if (int_n >= int_k) {
-                    int int_nCk = choose(int_n, int_k);
-                    string_output = String.valueOf(int_nCk);
+                if (BigInteger_n.compareTo(BigInteger_k) >= 0) {
+                    BigInteger BigInteger_nCk = choose(BigInteger_n, BigInteger_k);
+                    string_output = String.valueOf(BigInteger_nCk);
                 } else {
                     string_output = "0";
                 }
@@ -52,13 +53,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public int choose(int n, int k) {
-        if (k == 0) {
-            return 1;
+    public BigInteger choose(BigInteger n, BigInteger k) {
+        if (k.compareTo(BigInteger.ZERO) == 0) {
+            return new BigInteger("1");
         }
-        if (k > n/2) {
-            return choose(n, n-k);
+        if (k.compareTo(n.divide(new BigInteger("2"))) == 1) {
+            return choose(n, n.subtract(k));
         }
-        return n * choose(n-1, k-1) / k;
+        return n.multiply(choose(n.subtract(BigInteger.ONE), k.subtract(BigInteger.ONE))).divide(k);
     }
 }
